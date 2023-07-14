3ffacff3d5e8870ec5860bf5c82b3cc0780d4eef
Rei Sikk <reix0010@stud.kea.dk>
2023-07-14 18:37:27 +0200

    structure and sass set up

---
+++ src/Routes/App.jsx
@@ -0,0 +1,17 @@
+import Footer from "../Components/Footer";
+import NavBar from "../Components/NavBar";
+import "../sass/style.scss";
+
+function App() {
+
+
+  return (
+   <div className="layout-wrapper">
+    <NavBar />
+    <h1>Hello</h1>
+   <Footer />
+   </div>
+  )
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./Routes/App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from './Pages/HomePage';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+  
+      {
+        path: "/home",
+        element: <HomePage />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
