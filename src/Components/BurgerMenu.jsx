03967877d9794a4b60844edb89a36b29c982c1db
Rei Sikk <reix0010@stud.kea.dk>
2023-07-17 13:47:04 +0200

    burger menu almost done

--- src/Components/BurgerMenu.jsx
+++ src/Components/BurgerMenu.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import linkedinLogo from "../media/linkedIn.svg";
+import githublogo from "../media/github.svg";
 
 const BurgerMenu = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
@@ -24,6 +26,26 @@ const BurgerMenu = () => {
                     <li className='burger-link'>About</li>
                     <li className='burger-link'>Projects</li>
                 </ul>
+            </div>
+            <div className="menu-bottom">
+                <div className="menu-bottom-icons">
+                    <ul>
+                    <li className="linkedin-btn">
+                        <a target='_blank'
+            rel='noopener noreferrer' href="https://www.linkedin.com/in/rei-sikk-a642b9218/">
+                <img src={linkedinLogo} alt="LinkedIn logo" />
+            </a>
+                    </li>
+                    <li className="linkedin-btn">
+                        <a target='_blank'
+            rel='noopener noreferrer' href="https://github.com/ReiSikk">
+                <img src={githublogo} alt="Github logo" />
+            </a>
+                    </li>
+                    </ul>
+                </div>
+                <a  className="email-link" href="mailto:sikkrei@gmail.com">sikkrei@gmail.com</a>
+
             </div>
       </div>
     </div>
