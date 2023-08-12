de560e45b40fc7bca7caebf74260a726db9b19e9
Rei Sikk <reix0010@stud.kea.dk>
2023-08-12 15:29:47 +0200

    hero section

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import  reiPhoto from "../media/rei.svg";
 
 function HomePage() {
   return (
@@ -7,6 +8,9 @@ function HomePage() {
             <div className="hero-text">
                 <h1>Rei Sikk</h1>
                 <h2 className="h1-style">Web Designer/Developer</h2> 
+            </div>
+            <div className="hero-media">
+                <img src={reiPhoto} alt="illustration of Rei, the author of the portfolio website" />
             </div>
         </div>
     </header>
