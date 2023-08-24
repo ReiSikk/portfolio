156cad24c4def5dd7543218b4bbe96c40baa9a9d
Rei Sikk <reix0010@stud.kea.dk>
2023-08-24 19:02:45 +0200

    projects page init

---
+++ src/Pages/ProjectsPage.jsx
@@ -0,0 +1,21 @@
+
+
+function ProjectsPage() {
+  return (
+    <>
+    <header>
+        <h1>Projects</h1>
+        <h2 className="h4-style">UI / UX / Web Development</h2>
+    </header>
+    <main>
+        <section className="projects-grid">
+            <article className="project-item">
+                
+            </article>
+        </section>
+    </main>
+    </>
+  )
+}
+
+export default ProjectsPage
