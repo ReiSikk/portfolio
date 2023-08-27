e6b46e735f7cc14e72c432091fc0ce33db9fbab3
Rei Sikk <reix0010@stud.kea.dk>
2023-08-27 17:35:44 +0200

    project grid skeleton and responsivness done

--- src/Pages/ProjectsPage.jsx
+++ src/Pages/ProjectsPage.jsx
@@ -1,5 +1,6 @@
+import placeholder from '../media/placeholder.png'
+import arrow from '../media/arrowup.svg'
 
-import jsonData from '../projects.json'
 
 function ProjectsPage() {
   return (
@@ -10,25 +11,50 @@ function ProjectsPage() {
     </header>
     <main>
         <section className="projects-grid">
-            {jsonData.map(project => {
-                return (
-                    <article key={project.id} className={project.name}>
-                        <div className="article-top">
-                            <img src={project.image} alt={project.name} />
+                    <article  className="img-right">
+                        <div className="article-right">
+                            <img src={placeholder} alt="" />
                         </div>
-                        <div className="article-bottom">
-                           <h3 className="project-title">{project.name}</h3>
-                           <div className="labels">
-
+                        <div className="article-left">
+                           <h3 className="project-title">Project title</h3>
+                           <div className="labels-cont">
+                            <label className='label'>School project</label>
                            </div>
+                           <p className="project-desc">
+                           Re-designing and re-creating the Silfen Studios website
+                           to better highlight the company’s CSR efforts and to make their website better overall. 
+                           </p>
                            <div className="tech-stack">
-                            
+                            Tech stack here
                            </div>
-                           <p className="project-desc">{project.description}</p>
+                           <div className="article-links">
+                                <a href="">Live project<span><img src={arrow} alt="arrow for link element" className='arrow-up' /></span></a>
+                                <a href="">Github<span><img src={arrow} alt="arrow for link element" className='arrow-up' /></span></a>
+                           </div>
                         </div>
                     </article>
-                )
-            })}
+                    <article  className="img-left">
+                        <div className="article-left">
+                            <img src={placeholder} alt="" />
+                        </div>
+                        <div className="article-info">
+                           <h3 className="project-title">Project title</h3>
+                           <div className="labels-cont">
+                            <label className='label'>School project</label>
+                           </div>
+                           <p className="project-desc">
+                           Re-designing and re-creating the Silfen Studios website
+                           to better highlight the company’s CSR efforts and to make their website better overall. 
+                           </p>
+                           <div className="tech-stack">
+                            Tech stack here
+                           </div>
+                           <div className="article-links">
+                                <a href="">Live project<span><img src={arrow} alt="arrow for link element" className='arrow-up' /></span></a>
+                                <a href="">Github<span><img src={arrow} alt="arrow for link element" className='arrow-up' /></span></a>
+                           </div>
+                        </div>
+                    </article>
         </section>
     </main>
     </>
